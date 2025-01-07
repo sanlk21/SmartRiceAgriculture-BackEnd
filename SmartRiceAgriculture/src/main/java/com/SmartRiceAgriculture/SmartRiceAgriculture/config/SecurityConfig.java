03d6186564edf0ab3b65a08ca351a0cdd1845e10
sanlk21 <sandunlakmal2001@gmail.com>
2025-01-07 19:31:32 +0530

    user reg&logok

--- SmartRiceAgriculture/src/main/java/com/SmartRiceAgriculture/SmartRiceAgriculture/config/SecurityConfig.java
+++ SmartRiceAgriculture/src/main/java/com/SmartRiceAgriculture/SmartRiceAgriculture/config/SecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -21,10 +24,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf((csrf) -> csrf.disable())
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+                    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    config.setAllowedHeaders(Arrays.asList("*"));
+                    config.setAllowCredentials(true);
+                    return config;
+                }))
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/api/users/**").permitAll()
-                                .anyRequest().authenticated()
+                        auth.requestMatchers("/api/**").permitAll()
                 );
 
         return http.build();
