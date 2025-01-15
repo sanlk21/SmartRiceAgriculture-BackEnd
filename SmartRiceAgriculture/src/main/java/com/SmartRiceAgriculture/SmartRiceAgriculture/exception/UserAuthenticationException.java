59e0fadc180cfd10624108b7c565ee2ec34b68ef
sanlk21 <sandunlakmal2001@gmail.com>
2025-01-15 16:38:12 +0530

    upexcep

---
+++ SmartRiceAgriculture/src/main/java/com/SmartRiceAgriculture/SmartRiceAgriculture/exception/UserAuthenticationException.java
@@ -0,0 +1,13 @@
+package com.SmartRiceAgriculture.SmartRiceAgriculture.exception;
+
+
+
+public class UserAuthenticationException extends RuntimeException {
+    public UserAuthenticationException(String message) {
+        super(message);
+    }
+
+    public UserAuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
