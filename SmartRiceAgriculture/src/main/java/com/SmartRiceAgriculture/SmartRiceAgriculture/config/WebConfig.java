345f3331b5c3d5044138da0360ecc7f255eff7b7
sanlk21 <sandunlakmal2001@gmail.com>
2025-01-16 14:37:18 +0530

    portchange

--- SmartRiceAgriculture/src/main/java/com/SmartRiceAgriculture/SmartRiceAgriculture/config/WebConfig.java
+++ SmartRiceAgriculture/src/main/java/com/SmartRiceAgriculture/SmartRiceAgriculture/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Add your frontend URL
+                .allowedOrigins("http://localhost:5173") // Add your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- SmartRiceAgriculture/src/main/java/com/SmartRiceAgriculture/SmartRiceAgriculture/controller/UserController.java
+++ SmartRiceAgriculture/src/main/java/com/SmartRiceAgriculture/SmartRiceAgriculture/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     private final UserService userService;
